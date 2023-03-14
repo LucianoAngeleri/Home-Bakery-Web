b9b8cafc73b168358acbbde66aa2ef8ac9451cae
Luciano Angeleri <lcnang@gmail.com>
2023-03-14 19:47:24 -0300

    Se cmodificaron los modelos

--- HomeBakery/models.py
+++ HomeBakery/models.py
@@ -9,19 +9,26 @@ class Cliente(models.Model):
     fecha_nacimiento = models.DateField()
 
     def __str__(self):
-        return f'{self.id} - {self.nombre} {self.apellido}'
+        return f'Cliente ID:{self.id} - {self.nombre} {self.apellido}'
 
 class Producto(models.Model):
     nombre_producto = models.CharField(max_length=50)
     precio_producto = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Producto ID:{self.id} - {self.nombre_producto} - $ {self.precio_producto} - {self.cantidad_producto} un.'
+
+class Producto_Pedido(models.Model):
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    pedido = models.ForeignKey('Pedido', on_delete=models.CASCADE)
     cantidad_producto = models.IntegerField()
 
     def __str__(self):
-        return f'{self.id} - {self.nombre_producto} - $ {self.precio_producto} - {self.cantidad_producto} un.'
+        return f'{self.producto} x {self.cantidad} en {self.pedido}'
 class Pedido(models.Model):
     fecha_pedido = models.DateField()
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    productos_en_pedido = models.ManyToManyField(Producto)
+    productos= models.ManyToManyField(Producto,through=Producto_Pedido)
 
     def __str__(self):
-        return f'{self.id} - {self.cliente} - Fecha:{self.fecha_pedido}'
+        return f'Pedido ID:{self.id} - {self.cliente} - Fecha:{self.fecha_pedido}'
